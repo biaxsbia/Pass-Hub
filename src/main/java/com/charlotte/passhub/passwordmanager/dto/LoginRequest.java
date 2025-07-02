b0aca654814afee73c795b6aee9e41a53bf27a45
biaxsbia <beatrizsousaot@gmail.com>
2025-07-02 19:11:31 -0300

    Revert "wip"
    This reverts commit 93acc56e8a35029a96c0eb062dea00c83942260a.

--- src/main/java/com/charlotte/passhub/passwordmanager/dto/LoginRequest.java
+++ src/main/java/com/charlotte/passhub/passwordmanager/dto/LoginRequest.java
@@ -1,10 +1,8 @@
 package com.charlotte.passhub.passwordmanager.dto;
 import lombok.Data;
 import lombok.Getter;
-import lombok.Setter;
 
 
-@Setter
 @Getter
 @Data
 class LoginRequest {
@@ -12,6 +10,17 @@ class LoginRequest {
     private String password;
     private int totpCode;
 
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setTotpCode(int totpCode) {
+        this.totpCode = totpCode;
+    }
 }
 
 
