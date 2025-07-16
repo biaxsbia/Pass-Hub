c2ab1b871b235810385f3cb5c70eb0147d77327d
biaxsbia <beatrizsousaot@gmail.com>
2025-07-16 18:18:48 -0300

    wip

--- src/main/java/com/charlotte/passhub/passwordmanager/security/SecurityConfig.java
+++ src/main/java/com/charlotte/passhub/passwordmanager/security/SecurityConfig.java
@@ -29,9 +29,9 @@ public class SecurityConfig {
                 .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
-                                "/v3/api-docs/**",
-                                "/swagger-ui/**",
-                                "/swagger-ui.html",
+//                                "/v3/api-docs/**",
+//                                "/swagger-ui/**",
+//                                "/swagger-ui.html",
                                 "/auth/**",
                                 "/auth/**"
 //                                "/passwords/**"
